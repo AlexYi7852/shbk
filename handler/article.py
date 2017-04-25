4a6882fee48b78de732227ffff98e6233c6dce75
AlexYi <3092342855@qq.com>
2017-04-25 21:25:51 +0800

    add article

--- handler/article.py
+++ handler/article.py
@@ -14,7 +14,7 @@ class ApiSubmissionHandler(tornado.web.RequestHandler):
         if uid is None or uid == '':
             self.write('你还没登录')
             return
-        db = torndb.Connection(host="localhost", database="alex", user="root",password="11111111")
+        db = torndb.Connection(host="localhost", database="alex", user="root", password="11111111", time_zone='+8:00')
         user = db.get("select id from user where id=%s", uid)
         if user is None:
             db.close()
@@ -28,7 +28,7 @@ class ApiSubmissionHandler(tornado.web.RequestHandler):
 
 class CommentHandler(tornado.web.RequestHandler):
     def get(self, article_id):
-        db = torndb.Connection(host="localhost", database="alex", user="root", password="11111111")
+        db = torndb.Connection(host="localhost", database="alex", user="root", password="11111111", time_zone='+8:00')
         article = db.get('select * from article where id=%s',article_id)
         user = db.get('select * from user where id=%s', article.uid)
         comments = db.query('select * from comment where article_id=%s', article_id)
@@ -37,5 +37,14 @@ class CommentHandler(tornado.web.RequestHandler):
             user_info = db.get('select * from user where id=%s', uid)
             comment['user_info'] = user_info
         db.close()
-        self.render('comment.html', comments=comments,article=article,user=user)
+        self.render('comment.html', comments=comments, article=article, user=user)
 
+class ApiCommentHandler(tornado.web.RequestHandler):
+    def post(self):
+        article_id = self.get_argument('article_id')
+        content = self.get_argument('content')
+        uid = self.get_cookie('uid')
+        db = torndb.Connection(host='localhost', database='alex', user='root', password="11111111", time_zone='+8:00')
+        data = db.insert('insert into comment values (%S,%s,%s,%s,%s)', None, content, None, article_id, uid)
+        db.close()
+        self.redirect('/')
--- shbk.py
+++ shbk.py
@@ -1,4 +1,5 @@
 #coding: utf8
+
 import tornado.httpserver
 import tornado.options
 import tornado.ioloop
@@ -7,26 +8,28 @@ import os
 from tornado.options import define, options
 define("port", default=5010, help="run on the given port", type=int)
 
-from handler.index import HotHandler, LoginHandler, ApiLoginHandler,RegisterHandler, ApiRegisterHandler
-from handler.article import SubmissionHandler, ApiSubmissionHandler
+from handler.index import HotHandler, LoginHandler, ApiLoginHandler, RegisterHandler, ApiRegisterHandler
+from handler.article import SubmissionHandler, ApiSubmissionHandler, CommentHandler, ApiCommentHandler
 
 settings = {
     'debug' : True,
-    'template_path' : os.path.join(os.path.dirname(__file__),"front"),
-    "static_path" : os.path.join(os.path.dirname(__file__), "static")
+    'template_path': os.path.join(os.path.dirname(__file__), "front"),
+    "static_path": os.path.join(os.path.dirname(__file__), "static")
 }
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(
-        handlers = [
+        handlers=[
             (r"/", HotHandler),
             (r"/login", LoginHandler),
             (r"/api/login",ApiLoginHandler),
             (r"/register", RegisterHandler),
             (r"/api/register", ApiRegisterHandler),
             (r"/submission", SubmissionHandler),
-            (r"/api/submission", ApiSubmissionHandler)
+            (r"/api/submission", ApiSubmissionHandler),
+            (r"/comment", CommentHandler),
+            (r"/api/comment", ApiCommentHandler)
         ], **settings)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
